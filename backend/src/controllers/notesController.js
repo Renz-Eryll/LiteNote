c8b8837caf0e40f5f064aa7e0bf42d9381844fe1
Renz-Eryll <renzeryll09@gmail.com>
2025-07-12 12:56:21 +0800

    controllers

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,3 +1,15 @@
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
 export function getAllNotes(req, res) {
   res.status(200).send("You just fetched the notes");
 }
